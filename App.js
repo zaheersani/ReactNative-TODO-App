31b5113af26a4a82529db778ef32a4d1e46d91e2
zaheersani <sani.zaheer@gmail.com>
2020-04-29 13:23:43 +0500

    Get input from text and displaying

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, Button, View, TextInput, ScrollView } from 'react-native';
+
+export default function App() {
+  const [getText, setText] = useState('');
+  const [getList, setList] = useState(['item 1', 'item 2']);
+
+  const addItem = () => {
+    console.log(getText);
+    setText('');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>todo</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter Item"
+          onChangeText={text => setText(text)}
+          value={getText}
+        />
+        <Button
+          title="Add"
+          onPress={addItem}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 26 }}>{getText}</Text>
+      </View>
+      <ScrollView>
+        {getList.map((item) => { <Text>{item}</Text> })}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 64,
+    color: 'lightgrey'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 40
+  },
+  inputContainer: {
+    flexDirection: "row",
+    width: '70%',
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  textInput: {
+    borderColor: 'red',
+    //borderWidth: 2,
+    borderBottomWidth: 2,
+    width: '70%',
+    // borderRadius: 50,
+    fontSize: 16,
+    padding: 10
+  }
+});
